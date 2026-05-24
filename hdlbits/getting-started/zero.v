26073ccd7e731ad66b279fc042d9186adf540649
Udit8348 <usubramanya91@gmail.com>
2026-05-23 23:58:48 -0400

    start hdlbits

---
+++ hdlbits/getting-started/zero.v
@@ -0,0 +1,11 @@
+/* assign the output to zero */
+
+
+// Verilog-2001 ANSI-style port declaration syntax
+module top_module(
+    output zero
+);
+
+    assign zero = 0;
+
+endmodule
---
+++ hdlbits/readme.md
@@ -0,0 +1,3 @@
+# HDLBits
+
+Repo of Verilog Practice Exercises from the HDLBits Curriculum. Good practice to help memorize core syntax and constructs.
